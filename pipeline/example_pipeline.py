2efe70f02750068aa4f72bc48db0d7b42fb07eeb
Berna-96 <matteobernardini8796@gmail.com>
2026-02-13 12:12:59 +0100

    Create example_pipeline.py

---
+++ pipeline/example_pipeline.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from rules.validation_rules import validate_age, validate_weight
+
+def run_pipeline():
+
+    df = pd.read_csv("../data/patients.csv")
+
+    # Conversione tipi
+    df["age"] = pd.to_numeric(df["age"], errors="coerce")
+    df["weight"] = pd.to_numeric(df["weight"], errors="coerce")
+    df["dosage_mg"] = pd.to_numeric(df["dosage_mg"], errors="coerce")
+
+    # Validazioni
+    df = validate_age(df)
+    df = validate_weight(df)
+
+    # Flags di integrità (PARTE IMPORTANTE)
+    df["weight_missing_flag"] = df["weight"].isna()
+    df["record_quality"] = df["weight_missing_flag"].map(
+        {True: "incomplete_but_usable", False: "complete"}
+    )
+
+    print(df)
+
+if __name__ == "__main__":
+    run_pipeline()
